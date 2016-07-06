8fb01a6e65450b8e4abf44cfc5545f0679a25bca
Karen Liu <karenliu1@gmail.com>
2016-07-05 19:54:58 -0700

    Fix ID generation of stored values.

--- src/Storage.js
+++ src/Storage.js
@@ -2,6 +2,13 @@ import { AsyncStorage } from 'react-native';
 
 const SESSIONS_KEY = 'checkins';
 
+function getUniqueId(sessions) {
+  if (sessions.length === 0) {
+    return 1;
+  }
+  return Math.max(...sessions.map((s) => s.id)) + 1;
+}
+
 export default class Storage {
   static async getSessions() {
     let sessions;
@@ -16,19 +23,21 @@ export default class Storage {
 
   static async createSession(startTime, endTime, notes) {
     const sessions = await Storage.getSessions();
-
-    sessions.push({
-      id: sessions.length,
+    const session = {
+      id: getUniqueId(sessions),
       startTime,
       endTime,
       notes
-    });
+    };
+    sessions.push(session);
 
     try {
       await AsyncStorage.setItem(SESSIONS_KEY, JSON.stringify(sessions));
     } catch (error) {
       throw new Error('Could not save sessions:', error);
     }
+
+    return session;
   }
 
 
