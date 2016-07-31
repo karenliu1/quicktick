4e1eacf896ca6c5d56f8f9fffb67d1aa5bb5966f
Karen Liu <karenliu1@gmail.com>
2016-07-31 00:59:04 -0700

    Remove unused reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import sessions from './sessions';
-import editingSession from './editingSession';
 
 export default combineReducers({
   sessions,
-  editingSession,
 })
