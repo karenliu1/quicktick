794b3fc77fe56a2f0c954c487b0ef593d49c12f1
Karen Liu <karenliu1@gmail.com>
2016-08-01 19:18:13 -0700

    Analyze Screen: Add ability to filter by tags.

--- src/components/TotalsScreen.js
+++ src/components/TotalsScreen.js
@@ -12,6 +12,7 @@ import { SessionPropType } from '../PropTypes';
 import { formatDateTime, formatTotalFromDuration } from '../Utilities';
 
 import SectionText from './SectionText';
+import TagList from './TagList';
 
 class TotalsScreen extends Component {
   static propTypes = {
@@ -24,7 +25,7 @@ class TotalsScreen extends Component {
     this.state = {
       startTime: moment().startOf('week').toDate(),
       endTime: new Date(),
-      filter: null,
+      tagsFilter: [],
     };
   }
 
@@ -44,11 +45,34 @@ class TotalsScreen extends Component {
     });
   }
 
+  onChangeTags = () => {
+    Actions.tagEditorScreen({
+      initialTags: this.state.tagsFilter,
+      onSelect: (tagsFilter) => this.setState({ tagsFilter }),
+    });
+  }
+
   render() {
-    const filteredSessions = this.props.sessions.filter((session) => (
-      session.startTime >= this.state.startTime &&
-        session.endTime <= this.state.endTime
-    ));
+    const filteredSessions = this.props.sessions.filter((session) => {
+      if (session.startTime < this.state.startTime ||
+          session.endTime > this.state.endTime) {
+        return false;
+      }
+
+      // No tags to filter by
+      if (this.state.tagsFilter.length === 0) {
+        return true;
+      }
+
+      // Check that at least one session tag matches at least one filter tag
+      for (tag of this.state.tagsFilter) {
+        if (session.tags.indexOf(tag) !== -1) {
+          return true;
+        }
+      }
+
+      return false; // No tags on the session match the filter
+    });
 
     const total = filteredSessions.reduce((currentTotal, session) => {
       const elapsedMs = moment(session.endTime).diff(moment(session.startTime));
@@ -70,6 +94,12 @@ class TotalsScreen extends Component {
             onEdit={ this.onChangeEndTime }
             style={ styles.section }
           />
+          <SectionText
+            titleText="Filter By"
+            onEdit={ this.onChangeTags }
+            style={ styles.section }>
+            <TagList tags={ this.state.tagsFilter } />
+          </SectionText>
           <SectionText
             isLarge
             titleText="Total Time"
